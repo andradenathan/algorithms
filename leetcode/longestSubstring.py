8951857eee8b50006b093c26c0ab82f48c4ea94e
Nathan Andrade <nathan.nhdsk@gmail.com>
2021-11-12 12:32:01 -0300

    the longestsubstring exercise fixed up

--- leetcode/longestSubstring.py
+++ leetcode/longestSubstring.py
@@ -1,11 +1,14 @@
-def lengthOfLongestSubstring(s: str) -> int:
-  if s == "": return 0
-  arr = []
-  for i in s:
-    if i not in arr:
-      arr.append(i)
-
-  return len(arr)
-
-# it still getting the subsequence in this case but not the substring
-print(lengthOfLongestSubstring("pwwkew"))
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s) < 2: return len(s)
+        left, count = 0,0
+        right = left+1
+        hashmap = {s[left]: left}
+        while right < len(s):
+            if s[right] in hashmap:
+                left = max(hashmap[s[right]]+1, left)
+            hashmap[s[right]] = right
+            count = max(count, right - left + 1)
+            right += 1
+  
+        return count
