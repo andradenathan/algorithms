02b310b4c265e0abab51bc409473fc4a1793aff7
Nathan Andrade <nathan.nhdsk@gmail.com>
2022-07-28 20:15:19 -0300

    refactor diagonal diff algorithm

--- hacker-rank/diagonalDifference.py
+++ hacker-rank/diagonalDifference.py
@@ -1,45 +1,9 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'diagonalDifference' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts 2D_INTEGER_ARRAY arr as parameter.
-#
-
 def diagonalDifference(arr):
   left = 0
   right = 0
-  res = 0
   dim = len(arr)
   for i in range(dim):
-    for j in range(dim):
-      if i == j:
-        left += arr[i][j]
-      if (i+j) == dim - 1:
-        right += arr[i][j]
+    left += arr[i][i]
+    right += arr[i][dim-i-1]
 
-  res = abs(left - right)
-  return res
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    n = int(input().strip())
-
-    arr = []
-
-    for _ in range(n):
-        arr.append(list(map(int, input().rstrip().split())))
-
-    result = diagonalDifference(arr)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+  return abs(left - right)
